bf11588bf6264cd8a4eeb395df736d333f20e4bf
Lane <LeiLungSing@gmail.com>
2023-12-13 12:12:27 +0800

    vue router demo

---
+++ assets/NotFoundView-z3f_VCXh.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,b as c}from"./index-w5bV-bkn.js";const n={},s=c("b",null,"404 Not Found",-1),_=[s];function r(a,d){return o(),t("div",null,_)}const i=e(n,[["render",r]]);export{i as default};
